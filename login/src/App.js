e95bb073fcb308a755f1768137c0557a5428c2cb
domzxc2341 <dominiquesimbillo01@gmail.com>
2022-08-06 20:15:54 +0800

    ye
    ye

--- login/src/App.js
+++ login/src/App.js
@@ -1,26 +1,32 @@
+import React, { useState } from "react";
 
+function Wow(props) {
+  let looks;
+  if (props.itsura) {
+    looks = <h1>Puntahan kita ngayon?</h1>;
+  } else {
+    looks = <h1>Ay may deliver pala ako next time nalang</h1>;
+  }
+  return <div>{looks}</div>;
+}
 
-function Allclass (props){
+function Labas(props) {
   return (
-   <div> {props.user.tools}
-      {props.user.kit}
-</div>
+    <div>
+      <button onClick={props.gumana} type="button">
+        Sige
+      </button>
+    </div>
   );
-  }
+}
 
-  function Wow (props) {
-    return (
-      <div>
-        <Allclass user={props.item} />
-      </div>
-    )
-  }
-
-
-
-function App (){
+function App() {
+  const [Ex, Setex] = useState(true);
   return (
-    <Wow />
+    <>
+      <Wow itsura={Ex} />
+      <Labas gumana={() => Setex(!Ex)} />
+    </>
   );
 }
 
